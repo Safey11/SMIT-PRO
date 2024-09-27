08b9dbaed417efab9c3e371c276b3db1b7a41e95
Adnan Muhammad <135821652+Safey11@users.noreply.github.com>
2024-09-28 02:02:50 +0500

    updated code

---
+++ src/router/index.jsx
@@ -0,0 +1,30 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import React from 'react'
+import Home from '../pages/Home'
+import About from '../pages/About'
+import Services from '../pages/Services'
+import Contact from '../pages/Contact'
+import Portfolio from '../pages/Portfolio'
+import Team from '../pages/Team'
+import Navbar from '../component/Navbar'
+
+const AppRouter = () => {
+  return (
+    <div>
+        <BrowserRouter>
+            <Navbar />
+        <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='about' element={<About />}/>
+        <Route path='services' element={<Services />}/>
+        <Route path='contact' element={<Contact />}/>
+        <Route path='portfolio' element={<Portfolio />}/>
+        <Route path='team' element={< Team/>}/>
+        </Routes>
+        </BrowserRouter>
+
+    </div>
+  )
+}
+
+export default AppRouter
